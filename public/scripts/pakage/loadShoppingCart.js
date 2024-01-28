c8e521f3dbad9bd0800c59ee04aa087b81ac1924
Setex10 <pedrovela955@gmail.com>
2024-01-28 16:47:16 -0600

    changes on styles in store page

--- public/scripts/pakage/loadShoppingCart.js
+++ public/scripts/pakage/loadShoppingCart.js
@@ -10,10 +10,25 @@ const loadShoopingCart = () => {
         bodyTableStore.innerHTML = '';
     }
     const items = JSON.parse(itemsInShoppingCart);
+
+    const deleteItemEvent = (e) => {
+        const id = e.target.parentNode.parentNode.querySelector('input').value;
+        const itemsInShoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+        const itemToDelete = itemsInShoppingCart.findIndex(item => item.id_item === id);
+        itemsInShoppingCart.splice(itemToDelete, 1);
+        localStorage.setItem('shoppingCart', JSON.stringify(itemsInShoppingCart));
+        loadShoopingCart();
+    }
+
     items.forEach(item => {
         const itemsRows = createTags({
             tag:'tr', subTag: [
-                {tag: 'td', textContent: item.name},
+                {tag: 'input', value: item.id_item, class: 'hidden'},
+                {tag: 'td', textContent: item.name, class: "w-full flex flex-col justify-center items-center text-center", subTag: {
+                    tag: 'button', textContent: 'Eliminar',
+                    class: 'bg-red-500 hover:bg-red-700 text-white text-xs font-bold p-2 m-1 rounded', 
+                    addEventListener: ['click', deleteItemEvent]
+                }},
                 {tag: 'td', textContent: item.unit},
                 {tag: 'td', textContent: item.amount},
                 {tag: 'td', textContent: item.quantity},
